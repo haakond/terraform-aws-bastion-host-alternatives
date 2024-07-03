d0c94896f78ba86ce6f05923c0caeb52afd2f70d
Håkon Eriksen Drange <haakondrange@gmail.com>
2024-07-03 21:05:09 +0200

    Update example.

--- examples/main.tf
+++ examples/main.tf
@@ -1,5 +1,10 @@
 # Find relevant commit hash from https://github.com/haakond/terraform-aws-bastion-host-alternatives/commits/main/
+#
+# Note: If you provision this module "manually", remove cloud9_instance_owner_arn.
+# If you provision this module through a CI/CD pipeline, specify the ARN for your IAM principal, otherwise the Cloud9 instance will not be visible in the AWS Console
+#
 module "bastion_host_alternatives_demo" {
-  source                    = "git::https://github.com/haakond/terraform-aws-bastion-host-alternatives.git?ref=c1e31b2287be1e3d0e4f6321b421f3a1da977945"
-  cloud9_instance_owner_arn = "arn:aws:sts::1234567890:assumed-role/AWSReservedSSO_AWSAdministratorAccess_08c3cfd6621628f7/chuck@norris.onmicrosoft.com"
+  source = "git::https://github.com/haakond/terraform-aws-bastion-host-alternatives.git?ref=c1e31b2287be1e3d0e4f6321b421f3a1da977945"
+  #cloud9_instance_owner_arn = "arn:aws:sts::1234567890:assumed-role/AWSReservedSSO_AWSAdministratorAccess_08c3cfd6621628f7/chuck@norris.onmicrosoft.com"
+  # Commented out on purpose, see comment above.
 }
